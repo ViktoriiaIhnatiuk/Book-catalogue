725f5f850e847dc45b8fa11f8a384ef9ffa77264
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-07-24 16:15:50 +0300

    first commit

---
+++ src/main/java/com/example/bookcatalogueapp/BookCatalogueAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookcatalogueapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCatalogueAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCatalogueAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookcatalogueapp/BookCatalogueAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookcatalogueapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCatalogueAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
